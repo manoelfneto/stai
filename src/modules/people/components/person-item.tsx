c0be6979d13c13886785a146d8b32497d52da11c
manoel <manoelbetmann@hotmail.com>
2022-12-26 19:46:44 -0300

    choose turnover index color

--- src/modules/people/components/person-item.tsx
+++ src/modules/people/components/person-item.tsx
@@ -15,13 +15,23 @@ export const PersonItem = ({
   department,
   turnover,
 }: PersonItenProps) => {
+  const turnoverPercent = 90
   return (
     <Tr>
       <Td>{companyTime}</Td>
       <Td>{age}</Td>
       <Td>{position}</Td>
       <Td>{department}</Td>
-      <Td color='blue.300'>{turnover}</Td>
+      <Td color={chooseColor(turnoverPercent)}>{turnover}%</Td>
     </Tr>
   )
+}
+
+function chooseColor(value: number) {
+  let color = ''
+  if (value <= 25) color = 'green'
+  else if (value > 25 && value <= 50) color = 'yellow'
+  else if (value > 50 && value <= 75) color = 'orange'
+  else if (value > 75) color = 'red'
+  return color
 }
